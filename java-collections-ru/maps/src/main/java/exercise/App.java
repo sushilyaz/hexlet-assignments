fbbd56f9d7339524ae255ae86e0cb37b118cc6bb
@hexlet/cli <support@hexlet.io>
2023-09-04 22:36:45 +0300

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -5,10 +5,19 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 public class App {
+    public static void main(String[] args) {
+        String sentence = "";
+        Map wordsCount = App.getWordCount(sentence);
+        System.out.println(wordsCount);
+        String result = App.toString(wordsCount);
+        System.out.println(result);
+    }
+
     // BEGIN
     public static Map getWordCount(String sentence) {
-        String[] res = sentence.split(" ");
         Map <String, Integer> wordCountMap = new HashMap<>();
+        if (sentence.isEmpty()) return wordCountMap;
+        String[] res = sentence.split(" ");
         var count = 0;
         for (int i = 0; i < res.length; i++) {
             count = 0;
@@ -23,9 +32,12 @@ public class App {
     public static String toString (Map wordCountMap) {
         Map<String, Integer> wordCountMapMain = new HashMap<>(wordCountMap);
         String str = "{";
-        for (Map.Entry<String, Integer> word : wordCountMapMain.entrySet()) {
-            str = str + "\n" + "  " + word.getKey() + ": " + word.getValue();
+        if (!wordCountMapMain.isEmpty()) {
+            for (Map.Entry<String, Integer> word : wordCountMapMain.entrySet()) {
+                str = str + "\n" + "  " + word.getKey() + ": " + word.getValue();
+            }
         }
+        else return "{}";
         str = str + "\n" + "}";
         return str;
     }
