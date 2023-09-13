f67bc1c352da35ca711e9fa5183afe9bdd5cfbcf
@hexlet/cli <support@hexlet.io>
2023-09-13 21:48:15 +0300

    submit java-collections-ru/lambdas

--- java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
+++ java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
@@ -7,30 +7,27 @@ import java.time.LocalDate;
 import java.util.stream.Collectors;
 
 // BEGIN
-class public Sorter{
+class Sorter {
+    public static List<String> takeOldestMans2(List<Map<String, String>> users) {
+        return users.stream()
+                .filter(user -> user.get("gender") == "male")
+                .sorted((user1, user2) -> {
+                    LocalDate date1 = LocalDate.parse(user1.get("birthday"));
+                    LocalDate date2 = LocalDate.parse(user2.get("birthday"));
+                    return date1.compareTo(date2);
+                })
+                .map(user -> user.get("name"))
+                .collect(Collectors.toList());
+    }
 
-public static List<String> takeOldestMans (List<Map<String, String>> users) {
-        users = users.stream()
-        .filter(user -> user.get("gender").equals("male"))
-        .collect(Collectors.toList());
+    // alternative solution
+    public static List<String> takeOldestMans(List<Map<String, String>> users) {
+        return users.stream()
+                .filter(map1 -> map1.get("gender").equals("male"))
+                .sorted(Comparator.comparingLong(map2 -> LocalDate.parse(map2.get("birthday")).toEpochDay()))
+                .map(map3 -> map3.get("name"))
+                .collect(Collectors.toList());
+    }
 
-        List<LocalDate> localDate = new ArrayList<>();
-        for (var name : users) {
-        localDate.add(LocalDate.parse(name.get("birthday")));
-        }
-        localDate.sort(new Comparator<LocalDate>() {
-@Override
-public int compare(LocalDate o1, LocalDate o2) {
-        return o1.compareTo(o2);
-        }
-        });
-        List<String> result = new ArrayList<>();
-        for (LocalDate name : localDate) {
-        for (var user : users) {
-        if (name.toString().equals(user.get("birthday"))) result.add(user.get("name"));
-        }
-        }
-        return result;
-        }
-        }
+}
 // END
